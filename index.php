185f69d0215b161d5c2aacc5a5f52c62efde9dfe
Oscar Castro <112106599+ocastrobeltran@users.noreply.github.com>
2025-03-25 22:59:28 -0500

    Enable error reporting in index.php for development

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 require 'vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 use App\Config\Database;
 use App\Middleware\AuthMiddleware;
